2e5134259e1c25c5ee6a76f26025ef953626a5a1
Hunter Stevens <onebree@gmail.com>
2016-06-24 11:29:53 -0400

    Limit service report by issue/ward

--- service_report.rb
+++ service_report.rb
@@ -6,7 +6,7 @@ require "pp"
 summary = Hash.new { |h,k| h[k] = { :opened => 0, :closed => 0} }
 
 CSV.foreach("scf-data.csv", :headers => true) do |e| 
-  next unless e["request_type_id"] == "117" 
+  next unless e["request_type_id"] == "117" && e["ward"] == "18"
 
   if e["created_at"].present?
     summary[Date.parse(e["created_at"]).monday][:opened] += 1 
@@ -20,10 +20,13 @@ end
 week   = Date.parse("2010-01-01").monday
 finish = Date.parse("2016-06-01").monday
 
+
+puts "week,opened,closed"
+
 while week < finish
   summary[week][:opened] += summary[week - 7][:opened]
   summary[week][:closed] += summary[week - 7][:closed]
 
-  puts "#{week}: #{summary[week][:opened]}, #{summary[week][:closed]}"
+  puts [week, summary[week][:opened], summary[week][:closed]].to_csv
   week += 7
 end
