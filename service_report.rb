7fe30741febdb4dbda63933ca5d27f0e5628adf7
Gregory Brown <gregory.t.brown@gmail.com>
2016-06-24 11:16:07 -0400

    Add service report

---
+++ service_report.rb
@@ -0,0 +1,29 @@
+require "csv"
+require "active_support"
+require 'active_support/core_ext'
+require "pp"
+
+summary = Hash.new { |h,k| h[k] = { :opened => 0, :closed => 0} }
+
+CSV.foreach("scf-data.csv", :headers => true) do |e| 
+  next unless e["request_type_id"] == "117" 
+
+  if e["created_at"].present?
+    summary[Date.parse(e["created_at"]).monday][:opened] += 1 
+  end
+
+  if e["closed_at"].present?
+    summary[Date.parse(e["closed_at"]).monday][:closed] += 1 
+  end
+end
+
+week   = Date.parse("2010-01-01").monday
+finish = Date.parse("2016-06-01").monday
+
+while week < finish
+  summary[week][:opened] += summary[week - 7][:opened]
+  summary[week][:closed] += summary[week - 7][:closed]
+
+  puts "#{week}: #{summary[week][:opened]}, #{summary[week][:closed]}"
+  week += 7
+end
